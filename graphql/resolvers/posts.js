c3ba7848c1da7bb1628b2d274685e903df4cf03a
moubella <sudofalcon@gmail.com>
2019-12-17 15:28:01 +0100

    added likeResolvers

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,6 @@
 const { AuthenticationError } = require('apollo-server');
+const { UserInputError } = require('apollo-server');
+
 
 const Post = require('../../models/Post');
 const checkAuth = require('../../util/check-auth');
@@ -60,6 +62,25 @@ module.exports = {
                 throw new Error('error');
             }
 
+        },
+        async likePost(_, { postId }, context){
+            const { username } = checkAuth(context);
+            const post = await Post.findById(postId);
+
+            if(post){
+                if(post.likes.find( like => like.username === username)){
+                    // Post already liked by user, unlike it
+                    post.likes = post.likes.filter( like => like.username !== username);
+                } else {
+                    // Post not liked, like it
+                    post.likes.push({
+                        username,
+                        createdAt: new Date().toISOString()
+                    })
+                }
+                await post.save();
+                return post;
+            } else throw new UserInputError('Post not found');
         }
     }
 }
