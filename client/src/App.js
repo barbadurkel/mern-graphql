cdc50d3966c4e7be0c49a174f46d129dc7f728f2
moubella <sudofalcon@gmail.com>
2019-12-18 11:43:23 +0100

    added links to menubar and default selection based on window.location.pathname

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'; 
+import { Container } from 'semantic-ui-react'
+
 import 'semantic-ui-css/semantic.min.css'
 import './App.css';
 
@@ -11,10 +13,12 @@ import Register from './pages/Register';
 function App() {
   return (
     <Router>
+     <Container>
       <MenuBar/>
-      <Route exact path='/' component={Home}/>
-      <Route exact path='/login' component={Login}/>
-      <Route exact path='/register' component={Register}/>
+        <Route exact path='/' component={Home}/>
+        <Route exact path='/login' component={Login}/>
+        <Route exact path='/register' component={Register}/>
+     </Container>
     </Router>
   );
 }
